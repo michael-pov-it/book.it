fd45779487632b70a658d98dde3315776d761a66
Mike Gordievsky <michael.pov.it@gmail.com>
2022-01-27 02:11:16 +0100

    separating files to shared packages

--- main.go
+++ main.go
@@ -3,51 +3,90 @@ package main
 import (
 	"fmt"
 	"strings"
+	"boot.it-app/shared"
 )
 
+const availableTickets uint = 10
+var appName = "BookIt"
+var appGoal = "Tickets"
+var remainingTickets uint = 10
+var bookings = []string{}
+
 func main() {
-	appName := "BookIt"
-	appGoal := "Tickets"
 
-	const availableTickets uint = 50
-	var remainingTickets uint = 50
-	
-	fmt.Printf("Welcome to the %v application!\n", appName)
-	fmt.Println("Book", appGoal, "in this app!")
+	greetUsers()
 
-	bookings := []string{}
+	for remainingTickets > 0 && len(bookings) < 50 {
+		
+		// user input
+		userFirstName, userLastName, userEmail, userTickets := getUserInput()
 
-	for {
-		var userFirstName string
-		var userLastName string
-		var userEmail string
-		var numberOfTickets uint
+		isValidName, isValidEmail, isValidTicketsNumber := shared.ValidateUserInput(userFirstName, userLastName, userEmail, userTickets, remainingTickets)
 
-		fmt.Println("Please, enter your First Name, Last Name and Email:")
-		fmt.Println(">>> Use Enter button on your keyboard after each value! <<<")
-		fmt.Scan(&userFirstName, &userLastName, &userEmail)
-		fmt.Println("Please, enter the number of tickets you want to book:")
-		fmt.Scan(&numberOfTickets)
-		if numberOfTickets > remainingTickets {
-			fmt.Printf("Sorry, just %v tickets available!\n", remainingTickets)
-			break
+		if !isValidName {
+			fmt.Println("Sorry, You have entered too short First Name or Last Name.\nTry it again:")
+			continue
+		} else if !isValidEmail {
+			fmt.Println("Sorry, You have entered incorrect Email.\nTry it again:")
+			continue
+		} else if !isValidTicketsNumber {
+			fmt.Printf("Sorry, just %v tickets available!\n Choose number between 0 and %v", remainingTickets)
+			continue
 		} else {
-			remainingTickets = remainingTickets - numberOfTickets
+			remainingTickets = remainingTickets - userTickets
 			bookings = append(bookings, userFirstName + " " + userLastName + "<" + userEmail + "> booked tickets")
 			
-			fmt.Println("Thank you,", userFirstName, userLastName, "for booking", numberOfTickets, "tickets!")
+			fmt.Println("Thank you,", userFirstName, userLastName, "for booking", userTickets, "tickets!")
 			fmt.Println("You will receive a confirmaiton email at", userEmail)
 			fmt.Println(remainingTickets, "of", availableTickets, "tickets are still available")
 
-			clientFirstNames := []string{}
-			for _, booking := range bookings {
-				var names = strings.Fields(booking)
-				clientFirstNames = append(clientFirstNames, names[0])
-			}
+			clientFirstNames := getFirstNames()
 
-			fmt.Printf("These are all our bookings:\n %v\n", clientFirstNames)
+			fmt.Printf("The first names of bookings are: %v\n", clientFirstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Sold out. C U later!")
+				break
+			}
 		}
 	}
 
+}
+
+
+// Functions
+func greetUsers() {
+	fmt.Printf("Welcome at %v!\n", appName)
+	fmt.Println("Book", appGoal, "in this app!")
+}
+
+func getFirstNames() []string {
+	clientFirstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		clientFirstNames = append(clientFirstNames, names[0])
+	}
+
+	return clientFirstNames
+}
+
+func getUserInput() (string, string, string, uint) {
+	var userFirstName string
+	var userLastName string
+	var userEmail string
+	var userTickets uint
+
+	fmt.Println("Please, enter your First Name:")
+	fmt.Scan(&userFirstName)
+
+	fmt.Println("Please, enter your Last Name:")
+	fmt.Scan(&userLastName)
+
+	fmt.Println("Please, enter your Email:")
+	fmt.Scan(&userEmail)
+
+	fmt.Println("Please, enter the number of tickets you want to book:")
+	fmt.Scan(&userTickets)
 	
+	return userFirstName, userLastName, userEmail, userTickets
 }
